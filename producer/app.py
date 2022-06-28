217bfbe249888bc9dae339fbc9962a1972861591
Carlos Henrique <carloshkayser@gmail.com>
2022-06-28 16:34:00 -0300

    Avoid reading the whole file

--- producer/app.py
+++ producer/app.py
@@ -10,7 +10,7 @@ import os
 # KAFKA_HOST = "kafka-cluster-kafka-brokers:9092"
 KAFKA_HOST = os.getenv("KAFKA_HOST", "kafka-cluster-kafka-bootstrap:9092")
 KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "to_predict")
-WAIT_TIME = float(os.getenv("WAIT_TIME", "0.100")) # seconds
+WAIT_TIME = float(os.getenv("WAIT_TIME", "0.100"))  # seconds
 
 # https://stackoverflow.com/a/3885198/7412570
 class DecimalEncoder(json.JSONEncoder):
@@ -20,31 +20,34 @@ class DecimalEncoder(json.JSONEncoder):
         return super(DecimalEncoder, self).default(o)
 
 
-def start_producing():
+def start_producing(reader):
 
     producer = KafkaProducer(bootstrap_servers=KAFKA_HOST)
 
-    while True:
-        # message_id = str(uuid.uuid4())
-        # message = {"request_id": message_id, "data": get_next_row()}
-        # message = {'request_id': message_id, 'data': json.loads(generate_fake_message())}
+    for chunk in reader:
 
-        message = get_next_row()
-        message["id"] = decimal.Decimal(message["id"])
+        # chunk is a pandas.DataFrame
+        # dropna() to remove rows with NaN
+        chunk = chunk.dropna()
 
-        message_json = json.dumps(message, cls=DecimalEncoder).encode("utf-8")
-        print(message_json)
+        for row in chunk.iterrows():
+            # transform in dict
+            message = row[1].to_dict()
+            message["id"] = decimal.Decimal(message["id"])
 
-        producer.send(KAFKA_TOPIC, message_json)
-        producer.flush()
+            message_json = json.dumps(message, cls=DecimalEncoder).encode("utf-8")
+            print(message_json)
 
-        print(
-            "\033[1;31;40m -- PRODUCER: Sent message with id {}\x1b[0m".format(
-                message["id"]
+            producer.send(KAFKA_TOPIC, message_json)
+            producer.flush()
+
+            print(
+                "\033[1;31;40m -- PRODUCER: Sent message with id {}\x1b[0m".format(
+                    message["id"]
+                )
             )
-        )
 
-        sleep(WAIT_TIME)
+            sleep(WAIT_TIME)
 
 
 if __name__ == "__main__":
@@ -52,28 +55,14 @@ if __name__ == "__main__":
     print("KAFKA_TOPIC", KAFKA_TOPIC)
     print("KAFKA_HOST", KAFKA_HOST)
 
-    # # read csv file without pandas
-    # with open("test.gz", "r") as f:
-    #     line = f.readline()
-    #     while line:
-    #         # check if there is nan or null values
-    #         if "nan" in line or "null" in line:
-    #             line = f.readline()
-    #             continue
-
-    #         print(line)
-
-    #         line = f.readline()
-
     file_path = "test.gz"
-    df = pd.read_csv(file_path)
-    df = df.dropna()
 
-    row_iterator = df.iterrows()
+    def get_next_chunk(file_path: str) -> pd.DataFrame:
+        with pd.read_csv(file_path, chunksize=10000) as reader:
+            for chunk in reader:
+                yield chunk
 
-    def get_next_row():
-        _, row = next(row_iterator)
-        return row.to_dict()
+    reader = get_next_chunk(file_path)
 
     print("Starting producing")
-    start_producing()
+    start_producing(reader)
